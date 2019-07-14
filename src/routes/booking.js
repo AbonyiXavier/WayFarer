5e124093a50f755277ef0ac02959dc442238bbbf
AbonyiXavier <francisabonyi@gmail.com>
2019-07-14 02:47:24 -0700

    ft(booking) View all bookings

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import passport from 'passport';
 
 import bookingController from '../controllers/booking';
 const router = express.Router();
 
 router.post('/bookings', bookingController.bookASeatOnATrip);
 
-router.get('/bookings', bookingController.viewAllBookings);
+router.get(
+  '/bookings',
+  passport.authenticate('jwt', { session: false }),
+  bookingController.viewAllBookings,
+);
 
 export default router;
