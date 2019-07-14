1b5b768a673484da93f78f669e2580e4350abc07
AbonyiXavier <francisabonyi@gmail.com>
2019-07-14 02:23:24 -0700

    ft(trip) Get all trips

--- src/routes/trip.js
+++ src/routes/trip.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import passport from 'passport';
 import tripController from '../controllers/trips';
 const router = express.Router();
 
 router.post('/trips', tripController.createTrip);
 
-router.get('/trips', tripController.getAllTrips);
+router.get(
+  '/trips',
+  passport.authenticate('jwt', { session: false }),
+  tripController.getAllTrips,
+);
 
 export default router;
